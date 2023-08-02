f6629be4f4cbd601c7ac33f70696092847cddcc1
pranav <pranav.narayan@appinventiv.com>
2023-08-02 10:34:58 +0530

    Updated Main Branch

--- dist/src/router/user/profile/getProfile.js
+++
@@ -1,9 +0,0 @@
-// import expess from 'express'
-// //controller
-// // import {getProfileController} from '../../../controller/user/getProfileController'
-// import {getProfileController} from '../../../controller/user controller/getProfile'
-// //Middleware
-// import {sessionCheck}  from '../../../middleware/sessions';
-// const routes=expess();
-// routes.get('/profile',sessionCheck,getProfileController)
-// export default routes
--- src/router/user-Activity/makeBid.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { sessionCheck } from '../../middleware/sessions';
-import { biddingOnProducts } from '../../controller/bidController';
-
-
-const routes = express();
-routes.post('/making-bid',sessionCheck,biddingOnProducts);
-export default routes;
